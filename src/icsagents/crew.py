7df5146872399814ec03d2ff0bfcfedc5feda891
Syed Abdullah Hassan <56790074+Abdullah12has@users.noreply.github.com>
2025-02-02 03:24:57 +0200

    updated the crew settings

--- src/icsagents/crew.py
+++ src/icsagents/crew.py
@@ -69,5 +69,6 @@ class Icsagents():
 			tasks=self.tasks, # Automatically created by the @task decorator
 			process=Process.sequential,
 			verbose=True,
+			memory=True
 			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
